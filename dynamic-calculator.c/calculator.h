8003c9c648e69e97ffd3a96cc5d326ab4b221bc3
chrismensaha <chrisanin76563@gmail.com>
2025-07-30 18:56:10 -0400

    Complete function declarations

---
+++ dynamic-calculator.c/calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+double add(double addend_a, double addend_b);
+double subtract(double minuend, double subtrahend);
+double multiply(double factor_a, double factor_b);
+double divide(double dividend, double divisor );
+
+#endif
