0a1f67959c7445136e9d53a1b425bfc6ae2e475e
chrismensaha <chrisanin76563@gmail.com>
2025-07-31 05:23:29 -0400

    Add calculation history

--- dynamic-calculator.c/main.c
+++ dynamic-calculator.c/main.c
@@ -3,46 +3,47 @@
 #include "calculator.c"
 
 int main(){
-    while (1){
+    int size=0, count=0;
     double *history=NULL;
-    double a=0,b=0;
-    char mode='0';
-    double result=0;
-    int error=0;
-    printf("Enter here:");
-    scanf("%lf%c%lf",&a,&mode,&b);
-        
     
+    while (1){        
+        double a=0,b=0,result=0;
+        char mode=0;
+        
+        printf("Enter here:");
+        scanf("%lf%c%lf",&a,&mode,&b);    
+
         result=0;
-        switch (mode)
-        {
-        case '+':
-            result=add(a,b);
-            break;
-        case '-':
-            result=subtract(a,b);
-            break;
-        case '*':
-            result=multiply(a,b);     
-            break;
-        case '/':
-            if (b !=0){
-                result=divide(a,b); 
+        switch (mode){
+            case '+':result=add(a,b);break;
+                case '-':result=subtract(a,b);break;                
+                case '*':result=multiply(a,b);break;     
+                case '/':
+                if (b !=0){
+                    result=divide(a,b); 
+                }
+                else{
+                    printf("Error! Division by zero\n");                    
+                }               
+                break;
+            default:
+                printf("Invalid\n");
+                continue;
             }
-            else{
-                printf("Error! Division by zero\n");
-                error=1;
-            }
-            
-            break;
-        default:
-            printf("Try again\n");
-            continue;
-        }
-        if (error!=1){printf("%.2f\n",result);}
-        else{printf("Try again\n");}
+
+        double *temp = realloc(history,(count+1)*sizeof(double));  
+        history=temp;
+        history[count++]=result;   
+        
+        printf("= %.2f\n",result);
+        printf("History:\n");
+        for (int i=0;i<count;i++) { 
+                    
+            printf("%.2f\n",history[i]);
+            } 
+                
         
     }
-    
+    free(history);
     return 0;
 }
