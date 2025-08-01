6166f1e49fea97f19053d35aaa447b5d9009d934
chrismensaha <chrisanin76563@gmail.com>
2025-08-01 02:44:30 -0400

    Add documentation to function definitions

--- dynamic-calculator.c/calculator.c
+++ dynamic-calculator.c/calculator.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include "calculator.h"
 
-double add( double addend_a, double addend_b){
+// function that adds two values together then returns the result
+double add(double addend_a, double addend_b){ 
     return addend_a + addend_b;
 }
-
+// function that subtracts two values then returns the result
 double subtract(double minuend, double subtrahend){
     return minuend - subtrahend;
 }
-
+// function that multiplies two values and returns the result
 double multiply(double factor_a, double factor_b){
     return factor_a * factor_b;
 }
-
+// function that divides two values and returns the result
 double divide(double dividend, double divisor ){
     return dividend / divisor;
 }
