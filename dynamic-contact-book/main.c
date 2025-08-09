c8d60cf60aa8e5cb22535c6a7832d818c909e01d
chrismensaha <chrisanin76563@gmail.com>
2025-08-09 05:09:43 -0400

    Add case 1 documentation

--- dynamic-contact-book/main.c
+++ dynamic-contact-book/main.c
@@ -3,33 +3,40 @@
 #include <string.h>
 #include "contact-book.c"
 
-
 /*
-Build a command-line contact book where users can:
-Add contacts (name, phone number)
-List all contacts
-Search for a contact by name
-Delete a contact
-Free all memory before exiting */
+* Main program for contact book management
+* Ability to add, list, and delete contacts
+* Utilizes dynamic memory allocation for storage flexibility
+*/
 
 int main(){
-    int mode=0, input_number=0, size=100, contactsize=0, delete_num=0, starting_num=10;
+    // mode: menu selection
+    // input_number: number of contacts to add
+    // size: max length for input strings
+    // contactsize: current number of contacts stored
+    // starting_num: initial number of contacts allocated
+    int mode=0, input_number=0, size=100, contactsize=0, starting_num=10;
     int c, user_exit;
-    Contact *contact;
+    Contact *contact; // Pointer to Contact structs array
     
+    // Initial memory allocation for contacts
     contact=calloc(starting_num,sizeof(Contact));
 
+    // Main loop for continuous input requests
     while (1){
         printf("Press [1] to Add a contact, [2] to List all contacts, or [3] to Delete a contact: ");
         scanf("%d",&mode);
         switch (mode)
         {
-        case 1:
+        case 1: // Add contacts
             printf("# of contacts to add: ");        
             scanf("%d",&input_number);
+
+            // Validate Input
             if (input_number<=0){
                 printf("INVALID INPUT! Try Again\n"); break;}
-            else {                
+            else {  
+                // Reallocate memory for new contacts              
                 Contact *temp=realloc(contact,(contactsize+input_number)*sizeof(Contact));
                 if (!temp) {
                     printf("Error! Memory allocation failed\n");
@@ -39,11 +46,15 @@ int main(){
 
                 int start_index = contactsize;
                 contactsize+=input_number;
-
-                while ((c = getchar()) != '\n' && c != EOF);  
+                
+                // Clear leftover newline
+                while ((c = getchar()) != '\n' && c != EOF); 
+                
+                // Loop to fill new contact entries
                 for(int i=start_index; i<start_index+input_number;i++){
                     contact[i].name=malloc(size*sizeof(char));
                     contact[i].number=malloc(size*sizeof(char));
+
                     printf("Enter name: ");             
                     fgets(contact[i].name,size,stdin);  
                     contact[i].name[strcspn(contact[i].name, "\n")] = '\0';  
@@ -52,26 +63,17 @@ int main(){
                     fgets(contact[i].number,size,stdin);            
                     contact[i].number[strcspn(contact[i].number, "\n")] = '\0';
 
-                    printf("You have entered: {%s, %s}\n", contact[i].name, contact[i].number);              
-                    printf("Press [1] to continue | Press [0] to exit: ");
-                    scanf("%d",&user_exit);
+                    printf("You have entered: Index [%d] {%s, %s}\n",i, contact[i].name, contact[i].number);              
 
-                    if (user_exit==0){ 
-                        for(int i=0;i<contactsize;i++){
-                            free(contact[i].name);
-                            free(contact[i].number);
-                        }
-                        return 0;}
-                    else continue;
                     }
                 }        
         
         break;        
-        case 2:
+        case 2: 
                 if (contactsize==0){ printf("Empty contactbook! Please add a contact first\n"); break;}
                 else{ 
                     for(int i=0;i<contactsize;i++){ 
-                        printf("Name: %s  |  Number: %s\n", contact[i].name, contact[i].number);}                                                                            
+                        printf("Index [%d] | {%s, %s}\n",i, contact[i].name, contact[i].number);}                                                                            
                     } break;
         case 3:
                 if (contactsize!=0){
@@ -105,6 +107,14 @@ int main(){
         default:
             break;
         }
+        printf("Press [1] to continue | Press [0] to exit: ");
+        scanf("%d",&user_exit);
+        if (user_exit==0){ 
+            for(int i=0;i<contactsize;i++){
+                free(contact[i].name);
+                free(contact[i].number);}
+        break;}
+        else continue;
         }
     free(contact);
     return 0;
